d62ed026d88bcc960baedf1731aafdc61db185d8
Jeremy P <jeremy21@gmail.com>
2015-11-06 16:30:04 -0800

    end of week

--- wp-content/themes/leredbread/contact.php
+++ wp-content/themes/leredbread/contact.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Template Name: About Page
+ * Template Name: Contact Page
  *
  * @package RED_Starter_Theme
  */
@@ -11,12 +11,12 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-			<!-- <?php while ( have_posts() ) : the_post(); ?> -->
+			<!-- <?php while ( have_posts() ) : the_post(); ?>
+
+				<?php get_template_part( 'template-parts/content-contact.php', 'page' ); ?>
+
+			<?php endwhile; // End of the loop. ?> -->
 <?php the_content(); ?>
-
-
-			<!-- <?php endwhile; // End of the loop. ?> -->
-
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
